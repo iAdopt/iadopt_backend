acb6f583cd63bfe3e98d19b50aee6aa031f186c6
Marc Guzmán <marcguzmanalbiol@gmail.com>
2021-08-31 16:42:24 +0200

    Added animal tags in database.

--- src/db/mock_data.sql
+++ src/db/mock_data.sql
@@ -10,23 +10,23 @@ VALUES ('Iadopt', 'dog', '2017-02-20', 'male', 'new', 2, 'Lorem ipsum...');
 INSERT INTO animals (name, species, birthdate, gender, status, location, description)
 VALUES ('Iadopt', 'cat', '2017-02-20', 'male', 'new', 40, 'Lorem ipsum...');
 
-INSERT INTO animals (name, species, birthdate, gender, status, location, description)
-VALUES ('Iadopt', 'dog', '2017-02-20', 'female', 'new', 24, 'Lorem ipsum...');
+INSERT INTO animals (name, species, birthdate, gender, status, location, description, tags)
+VALUES ('Iadopt', 'dog', '2017-02-20', 'female', 'new', 24, 'Lorem ipsum...', ARRAY ['docil', 'cariñoso', 'bueno']);
 
-INSERT INTO animals (name, species, birthdate, gender, status, location, description)
-VALUES ('Iadopt', 'cat', '2021-02-20', 'female', 'new', 2, 'Lorem ipsum...');
+INSERT INTO animals (name, species, birthdate, gender, status, location, description, tags)
+VALUES ('Iadopt', 'cat', '2021-02-20', 'female', 'new', 2, 'Lorem ipsum...', null);
 
 INSERT INTO animals (name, species, birthdate, gender, status, location, description)
 VALUES ('Iadopt', 'dog', '2021-02-20', 'female', 'new', 2, 'Lorem ipsum...');
 
-INSERT INTO animals (name, species, birthdate, gender, status, location, description)
-VALUES ('Iadopt', 'cat', '2017-02-20', 'female', 'new', 2, 'Lorem ipsum...');
+INSERT INTO animals (name, species, birthdate, gender, status, location, description, tags)
+VALUES ('Iadopt', 'cat', '2017-02-20', 'female', 'new', 2, 'Lorem ipsum...', ARRAY ['le gusta jugar']);
 
 INSERT INTO animals (name, species, birthdate, gender, status, location, description)
 VALUES ('Iadopt', 'dog', '2017-02-20', 'male', 'urgent', 20, 'Lorem ipsum...');
 
-INSERT INTO animals (name, species, birthdate, gender, status, location, description)
-VALUES ('Iadopt', 'cat', '2021-02-20', 'male', 'urgent', 2, 'Lorem ipsum...');
+INSERT INTO animals (name, species, birthdate, gender, status, location, description, tags)
+VALUES ('Iadopt', 'cat', '2021-02-20', 'male', 'urgent', 2, 'Lorem ipsum...', ARRAY ['agil']);
 
 INSERT INTO animals (name, species, birthdate, gender, status, location, description)
 VALUES ('Iadopt', 'dog', '2021-02-20', 'male', 'urgent', 20, 'Lorem ipsum...');
